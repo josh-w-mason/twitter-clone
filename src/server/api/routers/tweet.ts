d29cdf0e6187668108945e8ed2f2c252fa2c99ad
Josh Mason <josh.mason.music@gmail.com>
2023-07-06 19:11:07 +1200

    update tweet tRPC router

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -1,7 +1,43 @@
 import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 export const tweetRouter = createTRPCRouter({
+  infiniteFeed: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().optional(),
+        cursor: z
+          .object({
+            id: z.string(),
+            createdAt: z.date(),
+          })
+          .optional(),
+      })
+    )
+    .query(async ({ input: { limit = 10, cursor }, ctx }) => {
+      const currentUserId = ctx.session?.user.id;
+
+      const tweets = await ctx.prisma.tweet.findMany({
+        take: limit + 1,
+        cursor: cursor ? { createdAt_id: cursor } : undefined,
+        orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+        select: {
+          id: true,
+          content: true,
+          createdAt: true,
+          _count: { select: { likes: true } },
+          likes:
+            currentUserId == null
+              ? false
+              : { where: { userId: currentUserId } },
+          user: { select: { name: true, id: true, image: true } },
+        },
+      });
+    }),
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
     .mutation(async ({ input: { content }, ctx }) => {
