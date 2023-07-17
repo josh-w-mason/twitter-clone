519e23d282957754c5fd0015f69a181d87b1700f
Josh Mason <josh.mason.music@gmail.com>
2023-07-17 17:19:15 +1200

    get infinite tweet list component displaying

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -21,7 +21,7 @@ export const tweetRouter = createTRPCRouter({
     .query(async ({ input: { limit = 10, cursor }, ctx }) => {
       const currentUserId = ctx.session?.user.id;
 
-      const tweets = await ctx.prisma.tweet.findMany({
+      const data = await ctx.prisma.tweet.findMany({
         take: limit + 1,
         cursor: cursor ? { createdAt_id: cursor } : undefined,
         orderBy: [{ createdAt: "desc" }, { id: "desc" }],
@@ -37,6 +37,27 @@ export const tweetRouter = createTRPCRouter({
           user: { select: { name: true, id: true, image: true } },
         },
       });
+      let nextCursor: typeof cursor | undefined;
+      if (data.length > limit) {
+        const nextItem = data.pop();
+        if (nextItem != null) {
+          nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
+        }
+      }
+
+      return {
+        tweets: data.map((tweet) => {
+          return {
+            id: tweet.id,
+            content: tweet.content,
+            createdAt: tweet.createdAt,
+            likeCount: tweet._count.likes,
+            user: tweet.user,
+            likedByMe: tweet.likes?.length > 0,
+          };
+        }),
+        nextCursor,
+      };
     }),
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
