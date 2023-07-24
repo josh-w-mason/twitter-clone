46cc2de12ea8be264686bf3556828c6783471ad6
Josh Mason <josh.mason.music@gmail.com>
2023-07-24 18:10:17 +1200

    add back button on profile page

---
+++ src/pages/profiles/[id].tsx
@@ -0,0 +1,62 @@
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next";
+import Head from "next/head";
+import { ssgHelper } from "~/server/api/ssgHelper";
+import { api } from "~/utils/api";
+import ErrorPage from "next/error";
+import Link from "next/link";
+import { IconHoverEffect } from "~/components/IconHoverEffect";
+import { VscArrowLeft } from "react-icons/vsc";
+
+const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  id,
+}) => {
+  const { data: profile } = api.profile.getById.useQuery({ id });
+
+  if (profile == null || profile.name == null)
+    return <ErrorPage statusCode={404} />;
+
+  return (
+    <>
+      <Head>
+        <title>{`Twitter Clone ${profile.name}`}</title>
+      </Head>
+      <header className="sticky top-0 z-10 flex items-center border-b bg-white px-4 py-2">
+        <Link href=".." className="mr-2">
+          <IconHoverEffect>
+            <VscArrowLeft className="h-6 w-6" />
+          </IconHoverEffect>
+        </Link>
+      </header>
+      {profile.name}
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export async function getStaticProps(
+  context: GetStaticPropsContext<{ id: string }>
+) {
+  const id = context.params?.id;
+
+  if (id == null) {
+    return { redirect: { destination: "/" } };
+  }
+
+  const ssg = ssgHelper();
+  await ssg.profile.getById.prefetch({ id });
+
+  return { props: { trpcState: ssg.dehydrate(), id } };
+}
+
+export default ProfilePage;
