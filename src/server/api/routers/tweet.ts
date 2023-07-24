d0e923b1d7b08581ae865b07302bf453cea2660d
Josh Mason <josh.mason.music@gmail.com>
2023-07-24 13:32:48 +1200

    add 2 separate tabs for recent and following tweets

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -1,5 +1,8 @@
+import { type Prisma } from "@prisma/client";
+import { type inferAsyncReturnType } from "@trpc/server";
 import { z } from "zod";
 import {
+  type createTRPCContext,
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
@@ -9,6 +12,7 @@ export const tweetRouter = createTRPCRouter({
   infiniteFeed: publicProcedure
     .input(
       z.object({
+        onlyFollowing: z.boolean().optional(),
         limit: z.number().optional(),
         cursor: z
           .object({
@@ -18,47 +22,20 @@ export const tweetRouter = createTRPCRouter({
           .optional(),
       })
     )
-    .query(async ({ input: { limit = 10, cursor }, ctx }) => {
-      const currentUserId = ctx.session?.user.id;
-
-      const data = await ctx.prisma.tweet.findMany({
-        take: limit + 1,
-        cursor: cursor ? { createdAt_id: cursor } : undefined,
-        orderBy: [{ createdAt: "desc" }, { id: "desc" }],
-        select: {
-          id: true,
-          content: true,
-          createdAt: true,
-          _count: { select: { likes: true } },
-          likes:
-            currentUserId == null
-              ? false
-              : { where: { userId: currentUserId } },
-          user: { select: { name: true, id: true, image: true } },
-        },
-      });
-      let nextCursor: typeof cursor | undefined;
-      if (data.length > limit) {
-        const nextItem = data.pop();
-        if (nextItem != null) {
-          nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
-        }
+    .query(
+      async ({ input: { limit = 10, onlyFollowing = false, cursor }, ctx }) => {
+        const currentUserId = ctx.session?.user.id;
+        return await getInfiniteTweets({
+          limit,
+          ctx,
+          cursor,
+          whereClause:
+            currentUserId == null || !onlyFollowing
+              ? undefined
+              : { user: { followers: { some: { id: currentUserId } } } },
+        });
       }
-
-      return {
-        tweets: data.map((tweet) => {
-          return {
-            id: tweet.id,
-            content: tweet.content,
-            createdAt: tweet.createdAt,
-            likeCount: tweet._count.likes,
-            user: tweet.user,
-            likedByMe: tweet.likes?.length > 0,
-          };
-        }),
-        nextCursor,
-      };
-    }),
+    ),
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
     .mutation(async ({ input: { content }, ctx }) => {
@@ -84,3 +61,54 @@ export const tweetRouter = createTRPCRouter({
       }
     }),
 });
+
+async function getInfiniteTweets({
+  whereClause,
+  ctx,
+  limit,
+  cursor,
+}: {
+  whereClause?: Prisma.TweetWhereInput;
+  limit: number;
+  cursor: { id: string; createdAt: Date } | undefined;
+  ctx: inferAsyncReturnType<typeof createTRPCContext>;
+}) {
+  const currentUserId = ctx.session?.user.id;
+
+  const data = await ctx.prisma.tweet.findMany({
+    take: limit + 1,
+    cursor: cursor ? { createdAt_id: cursor } : undefined,
+    orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+    where: whereClause,
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+      _count: { select: { likes: true } },
+      likes:
+        currentUserId == null ? false : { where: { userId: currentUserId } },
+      user: { select: { name: true, id: true, image: true } },
+    },
+  });
+  let nextCursor: typeof cursor | undefined;
+  if (data.length > limit) {
+    const nextItem = data.pop();
+    if (nextItem != null) {
+      nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
+    }
+  }
+
+  return {
+    tweets: data.map((tweet) => {
+      return {
+        id: tweet.id,
+        content: tweet.content,
+        createdAt: tweet.createdAt,
+        likeCount: tweet._count.likes,
+        user: tweet.user,
+        likedByMe: tweet.likes?.length > 0,
+      };
+    }),
+    nextCursor,
+  };
+}
